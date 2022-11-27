2f99ff98770090fad86e595e97f5ab03327c303c
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2022-11-27 16:37:03 +0530

    Pong Game created and added

--- paddle.py
+++ paddle.py
@@ -6,7 +6,6 @@ class Element:
     def __init__(self, position):
         self.element = turtle.Turtle()
         self.element.speed(0)
-        self.element.shape("square")
         self.element.color("white")
         self.element.penup()
         self.element.goto(position, 0)
@@ -27,11 +26,28 @@ class Element:
 class Ball(Element):
     def __init__(self, position):
         super().__init__(position)
+
+        self.element.shape("circle")
         self.element.dx = 2
         self.element.dy = 2
 
 
 class Paddle(Element):
+
     def __init__(self, position):
         super().__init__(position)
+        self.element.shape("square")
         self.element.shapesize(stretch_wid=5, stretch_len=1)
+
+
+class Pen(Element):
+
+    def __init__(self, position):
+        super().__init__(position)
+        self.element.goto(0, 300)
+        self.element.write("Player A: 0   Player B : 0",
+                           align="center", font=("Courier", 24, "normal"))
+
+    def write_score(self, scorea, scoreb):
+        self.element.write(f"Player A: {scorea}   Player B : {scoreb}",
+                           align="center", font=("Courier", 24, "normal"))
--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import turtle
+import random
 
-from paddle import Paddle, Element, Ball
+from paddle import Paddle, Ball, Pen
 
 
 wn = turtle.Screen()
@@ -20,7 +21,8 @@ paddle_b = Paddle(400)
 # Ball
 ball = Ball(0)
 
-# functions
+# Score
+pen = Pen(0)
 
 
 # Keyboard Bindings
@@ -34,10 +36,44 @@ wn.onkeypress(paddle_a.move_down, "s")
 wn.onkeypress(paddle_b.move_up, "Up")
 wn.onkeypress(paddle_b.move_down, "Down")
 # Main Game loop
+player_1 = 0
+player_2 = 0
+ball.element.dx = random.choice([-2, 2])
+ball.element.dy = random.choice([-2, 2])
 
 while True:
     wn.update()
 
     # Move ball
 
-    ball.setx()
+    ball.element.setx(ball.element.xcor()+ball.element.dx)
+    ball.element.sety(ball.element.ycor()+ball.element.dy)
+
+    # Border check
+    if (ball.element.ycor() > 340):
+        ball.element.sety(340)
+        ball.element.dy *= -1
+    if (ball.element.ycor() < -340):
+        ball.element.sety(-340)
+        ball.element.dy *= -1
+    if (ball.element.xcor() > 440):
+        ball.element.goto(0, 0)
+        ball.element.dx = random.choice([-2, 2])
+        player_1 += 1
+        pen.element.clear()
+        pen.write_score(player_1, player_2)
+    if (ball.element.xcor() < -440):
+        ball.element.goto(0, 0)
+        ball.element.dx = random.choice([-2, 2])
+        player_2 += 1
+        pen.element.clear()
+        pen.write_score(player_1, player_2)
+    # collission
+
+    if (ball.element.xcor() > 390 and ball.element.xcor() < 450) and (ball.element.ycor() < paddle_b.element.ycor()+50 and ball.element.ycor() > paddle_b.element.ycor()-50):
+        ball.element.setx(390)
+        ball.element.dx *= -1
+
+    if (ball.element.xcor() < -390 and ball.element.xcor() > -450) and (ball.element.ycor() < paddle_a.element.ycor()+50 and ball.element.ycor() > paddle_a.element.ycor()-50):
+        ball.element.setx(-390)
+        ball.element.dx *= -1
